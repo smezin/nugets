7c454d2b456242a3a3b9aa9de5c1b7f639a09b95
shahafme <shahafme@matrix.co.il>
2022-04-07 07:46:06 +0300

    .

--- get_nugets.py
+++ get_nugets.py
@@ -6,14 +6,14 @@ def can_i_have_that_many(packs: List[int], i_want_that_many: int) -> List[bool]:
     param i_want_that_many: number of nuggets you want to buy
     rvalue: True you can buy the desired amount, False otherwise
     """
-    def gimme_nuggets(current_sum, i_want_that_many):
-        if current_sum > i_want_that_many or is_possible[current_sum]:
+    def gimme_nuggets(current_nuggets, i_want_that_many):
+        if current_nuggets > i_want_that_many or is_possible[current_nuggets]:
             return
        
-        is_possible[current_sum] = True
+        is_possible[current_nuggets] = True
 
         for pack in packs:
-            gimme_nuggets(current_sum + pack, i_want_that_many)
+            gimme_nuggets(current_nuggets + pack, i_want_that_many)
        
     is_possible = [False for _ in range(i_want_that_many + 1)]
     gimme_nuggets(0, i_want_that_many)
